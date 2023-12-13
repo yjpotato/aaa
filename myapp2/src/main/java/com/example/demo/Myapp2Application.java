61e1910b01bc21611d3fadd9987655ed915f34d1
kosa <kosa@DESKTOP-ANOQI3Q>
2023-12-13 11:39:27 +0900

    Initial Commit

---
+++ myapp2/src/main/java/com/example/demo/Myapp2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myapp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myapp2Application.class, args);
+	}
+
+}
